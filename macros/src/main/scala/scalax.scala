1e5a9bf1991a80eba675e28fca39ec0a54572398
Alois Cochard <alois.cochard@gmail.com>
2013-01-24 19:51:04 +0100

    Factorize macros code

--- macros/src/main/scala/scalax.scala
+++ macros/src/main/scala/scalax.scala
@@ -18,67 +18,21 @@ package object scalax {
   case class EnumDef(id: String, name: String)
 
   object Macros {
-    // TODO Factorize common macros code
 
     def enum(c: Context)(values: c.Tree*): c.Tree = {
       import c.universe._
       import Flag._
 
-      val enumDefs = values.toList.collect {
-        case Ident(TermName(id)) => EnumDef(id, id)
-        case Apply(Ident(TermName(id)), List(Literal(Constant(name)))) => EnumDef(id, name.toString)
-      }
-
-      val valueObjects = enumDefs.map { enumDef =>
-        ModuleDef(
-          Modifiers(),
-          TermName(enumDef.id),
-          Template(
-            List(Select(This(TypeName(c.enclosingImpl.name.toString)), TypeName("Val"))),
-            emptyValDef,
-            List(
-              DefDef(
-                Modifiers(), 
-                nme.CONSTRUCTOR, 
-                List(), 
-                List(List()), 
-                TypeTree(), 
-                Block(List(Apply(Select(Super(This(tpnme.EMPTY), tpnme.EMPTY), nme.CONSTRUCTOR), List())), Literal(Constant(())))
-              ), 
-              DefDef(Modifiers(), TermName("name"), List(), List(), TypeTree(), Literal(Constant(enumDef.name)))
-            )
-          )
-        )
-      }
-
-      val valuesList = ValDef(
-        Modifiers(),
-        TermName("values"),
-        AppliedTypeTree(Ident(TypeName("Seq")), List(Ident(TypeName("Value")))),
-        Apply(
-          Select(
-            Select(Select(Select(Ident(TermName("scala")), TermName("collection")), TermName("immutable")), TermName("List")),
-            TermName("apply")
-          ), 
-          enumDefs.map(enumDef => Ident(TermName(enumDef.id))).toList
-        )
-      )
-
+      val enumDefs = parseValues(c)(values.toList)
 
       val Expr(Block(List(valueSealedTrait), _)) = reify {
         sealed trait Val extends scalax.Value
       }
 
       val valueType = TypeDef(Modifiers(OVERRIDE), TypeName("Value"), List(), Ident(TypeName("Val")))
+      val valueTypeTree = Select(This(TypeName(c.enclosingImpl.name.toString)), TypeName("Val"))
 
-      val generatedCode = valueSealedTrait :: valueType :: valuesList :: valueObjects
-
-      val Expr(Block(List(ClassDef(_, _, _, Template(parents, self, body))), _)) = reify {
-        class CONTAINER extends Enumerable
-      }
-     
-      val Template(_, _, _ :: existingCode) = c.enclosingTemplate
-      Template(parents, emptyValDef, body ++ generatedCode ++ existingCode)
+      template(c)(valueSealedTrait :: valueType :: valuesList(c)(valueTypeTree, enumDefs) :: valueObjects(c)(valueTypeTree, enumDefs))
     }
 
 
@@ -89,46 +43,28 @@ package object scalax {
       implicit val context = c
 
       val tpe = c.weakTypeOf[T]
-      val names = values.map(_.toString)
+      val enumDefs = parseValues(c)(values.toList)
 
-      val valueObjects = names.toList.map { name =>
-        ModuleDef(
-          Modifiers(),
-          TermName(name),
-          Template(
-            List(Ident(tpe.typeSymbol)),
-            emptyValDef,
-            List(
-              DefDef(
-                Modifiers(), 
-                nme.CONSTRUCTOR, 
-                List(), 
-                List(List()), 
-                TypeTree(), 
-                Block(List(Apply(Select(Super(This(tpnme.EMPTY), tpnme.EMPTY), nme.CONSTRUCTOR), List())), Literal(Constant(())))
-              ), 
-              DefDef(Modifiers(), TermName("name"), List(), List(), TypeTree(), Literal(Constant(name)))
-            )
-          )
-        )
-      }
-
-      val valuesList = ValDef(
-        Modifiers(),
-        TermName("values"),
-        AppliedTypeTree(Ident(TypeName("Seq")), List(TypeTree(tpe))),
-        Apply(
-          Select(
-            Select(Select(Select(Ident(TermName("scala")), TermName("collection")), TermName("immutable")), TermName("List")),
-            TermName("apply")
-          ), 
-          names.map(name => Ident(TermName(name))).toList
-        )
-      )
-
+      val valueTypeTree = Ident(tpe.typeSymbol)
       val valueType = TypeDef(Modifiers(OVERRIDE), TypeName("Value"), List(), Ident(tpe.typeSymbol))
 
-      val generatedCode = valueType :: valuesList :: valueObjects
+      val generatedCode = valueType ::
+        valuesList(c)(valueTypeTree, enumDefs) ::
+        valueObjects(c)(valueTypeTree, enumDefs)
+
+      template(c)(valueType :: valuesList(c)(valueTypeTree, enumDefs) :: valueObjects(c)(valueTypeTree, enumDefs))
+    }
+
+    private def parseValues(c: Context)(xs: List[c.Tree]): List[EnumDef] = {
+      import c.universe._
+      xs.collect {
+        case Ident(TermName(id)) => EnumDef(id, id)
+        case Apply(Ident(TermName(id)), List(Literal(Constant(name)))) => EnumDef(id, name.toString)
+      }
+    }
+
+    private def template(c: Context)(generatedCode: List[c.Tree]) = {
+      import c.universe._
 
       val Expr(Block(List(ClassDef(_, _, _, Template(parents, self, body))), _)) = reify {
         class CONTAINER extends Enumerable
@@ -138,5 +74,43 @@ package object scalax {
       Template(parents, emptyValDef, body ++ generatedCode ++ existingCode)
     }
 
+    private def valueObjects(c: Context)(typeTree: c.Tree, enumDefs: List[EnumDef]): List[c.Tree] = enumDefs.map { enumDef =>
+      import c.universe._
+      ModuleDef(
+        Modifiers(),
+        TermName(enumDef.id),
+        Template(
+          List(typeTree),
+          emptyValDef,
+          List(
+            DefDef(
+              Modifiers(), 
+              nme.CONSTRUCTOR, 
+              List(), 
+              List(List()), 
+              TypeTree(), 
+              Block(List(Apply(Select(Super(This(tpnme.EMPTY), tpnme.EMPTY), nme.CONSTRUCTOR), List())), Literal(Constant(())))
+            ), 
+            DefDef(Modifiers(), TermName("name"), List(), List(), TypeTree(), Literal(Constant(enumDef.name)))
+          )
+        )
+      )
+    }
+
+    private def valuesList(c: Context)(typeTree: c.Tree, enumDefs: List[EnumDef]): c.Tree = {
+      import c.universe._
+      ValDef(
+        Modifiers(),
+        TermName("values"),
+        AppliedTypeTree(Ident(TypeName("Seq")), List(typeTree)),
+        Apply(
+          Select(
+            Select(Select(Select(Ident(TermName("scala")), TermName("collection")), TermName("immutable")), TermName("List")),
+            TermName("apply")
+          ), 
+          enumDefs.map(enumDef => Ident(TermName(enumDef.id))).toList
+        )
+      )
+    }
   }
 }
